ccd7297816d24532977d42a4b7c6ba63bf8c6e87
adicurious <adiakhare60@gmail.com>
2022-08-25 00:38:59 +0530

    add scrolling

--- jymspace-frontend/src/AppRouter/AppRouter.jsx
+++ jymspace-frontend/src/AppRouter/AppRouter.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import {lazy, Suspense} from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "../components/Pages/Home/Home";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "../Utility/ErrorBoundary";
+const Home = lazy(() => import ("../components/Pages/Home/Home"));
+
 
 const AppRouter = () => {
   return (
     <div>
+      <ErrorBoundary FallbackComponent={ErrorFallback} onReset={() => {}}>
+      <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        
       </Routes>
+      </Suspense>
+      </ErrorBoundary>
     </div>
   );
 };
