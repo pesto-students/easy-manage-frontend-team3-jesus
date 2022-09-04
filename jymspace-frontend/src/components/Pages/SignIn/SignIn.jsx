cbef1f511b8699f8a5b55ba6cfb2dd63af497100
adicurious <adiakhare60@gmail.com>
2022-09-04 13:46:33 +0530

    register form validation

--- jymspace-frontend/src/components/Pages/SignIn/SignIn.jsx
+++ jymspace-frontend/src/components/Pages/SignIn/SignIn.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Navbar from "../../Navbar/Navbar"
 import Footer from "../../Footer/Footer"
-import Form from "../../Form/FormS"
 
 const Signin = () => {
   return (
     <div>
      <Navbar/>
-     <Form/>
+     
      <Footer/>
     </div>
   )
